2b5075183cf07d0323463c65b1eb32cfacc5e94c
Thibault Ruby <thibault.ruby@neoxia.com>
2023-06-15 11:31:25 +0200

    chore: start adding utils tests

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,42 @@
+use regex::Regex;
+
 pub fn remove_first_and_last_chars(value: String) -> String {
     let mut chars = value.chars();
     chars.next();
     chars.next_back();
     chars.as_str().to_string()
+}
+
+pub fn is_internal_dep(dependency_string: &String) -> bool {
+    let re = Regex::new(r"^\.+").unwrap();
+    re.is_match(dependency_string)
+}
+
+#[cfg(test)]
+mod utils_tests {
+    use crate::ast_browser::utils::{is_internal_dep, remove_first_and_last_chars};
+
+    #[test]
+    fn remove_first_and_last_chars_test() {
+        let result = remove_first_and_last_chars(String::from("AAA"));
+        assert_eq!(result, "A");
+    }
+
+    #[test]
+    fn is_internal_dep_should_returns_true_with_local_dep_test() {
+        let result = is_internal_dep(&String::from("./aah"));
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn is_internal_dep_should_returns_true_with_out_dep_test() {
+        let result = is_internal_dep(&String::from("../../aah"));
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn is_internal_dep_should_returns_false_with_internal_test() {
+        let result = is_internal_dep(&String::from("@angular/core"));
+        assert_eq!(result, false);
+    }
 }
--- tests/functional/src/a.ts
+++
@@ -1,2 +0,0 @@
-import { baz } from 'foo';
-import { baz as bazbaz } from 'foobar';
