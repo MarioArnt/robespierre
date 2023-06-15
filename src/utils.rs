a5a3d55e3a4657ac0a1daab09a8cd48d258a84e6
Thibault Ruby <thibault.ruby@neoxia.com>
2023-06-15 13:17:44 +0200

    feat: utils are done and tested

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use regex::Regex;
 
 pub fn remove_first_and_last_chars(value: String) -> String {
@@ -12,15 +13,27 @@ pub fn is_internal_dep(dependency_string: &String) -> bool {
     re.is_match(dependency_string)
 }
 
+pub fn filtered_internal_deps(dependencies: &HashSet<String>) -> HashSet<&String> {
+    let filtered_deps = dependencies.into_iter()
+            .filter(|dependency| !is_internal_dep(dependency))
+            .collect();
+    filtered_deps
+}
+
 #[cfg(test)]
-mod utils_tests {
-    use crate::ast_browser::utils::{is_internal_dep, remove_first_and_last_chars};
+mod remove_first_and_last_chars_tests {
+    use crate::ast_browser::utils::remove_first_and_last_chars;
 
     #[test]
     fn remove_first_and_last_chars_test() {
         let result = remove_first_and_last_chars(String::from("AAA"));
         assert_eq!(result, "A");
     }
+}
+
+#[cfg(test)]
+mod is_internal_dep_tests {
+    use crate::ast_browser::utils::{is_internal_dep};
 
     #[test]
     fn is_internal_dep_should_returns_true_with_local_dep_test() {
@@ -38,5 +51,31 @@ mod utils_tests {
     fn is_internal_dep_should_returns_false_with_internal_test() {
         let result = is_internal_dep(&String::from("@angular/core"));
         assert_eq!(result, false);
+    }
+}
+
+#[cfg(test)]
+mod filtered_internal_deps_tests {
+    use std::collections::HashSet;
+    use crate::ast_browser::utils::filtered_internal_deps;
+
+    #[test]
+    fn filter_internal_deps_should_returns_same_test() {
+        let mut base_deps: HashSet<String> = HashSet::new();
+        let external_dep: String = String::from("@angular/core");
+        base_deps.insert(external_dep);
+        let result = filtered_internal_deps(&base_deps);
+        assert_eq!(result.len(), 1);
+    }
+
+    #[test]
+    fn filter_internal_deps_should_returns_filtered_test() {
+        let mut base_deps: HashSet<String> = HashSet::new();
+        let external_dep: String = String::from("@angular/core");
+        let internal_dep: String = String::from("./aah");
+        base_deps.insert(external_dep);
+        base_deps.insert(internal_dep);
+        let result = filtered_internal_deps(&base_deps);
+        assert_eq!(result.len(), 1);
     }
 }
