8f222cac2b5bf92bd1a0640e5f56ede3ae73e3f2
Thibault Ruby <thibault.ruby@neoxia.com>
2023-11-20 10:26:46 +0100

    fix: linting

--- src/write_report.rs
+++ src/write_report.rs
@@ -1,16 +1,14 @@
-use std::fs::{write};
 use serde_json::{json, to_string_pretty};
+use std::fs::write;
 
 const REPORT_FILE_NAME: &str = "robespierre_report.json";
 
 pub fn write_json_report(extraneous: Vec<&String>, implicit: Vec<&String>) {
     let json_to_write = json!({
-                "extraneous_dependencies": extraneous,
-                "implicit_dependencies": implicit,
-            });
+        "extraneous_dependencies": extraneous,
+        "implicit_dependencies": implicit,
+    });
 
-    write(
-        REPORT_FILE_NAME,
-        to_string_pretty(&json_to_write).unwrap(),
-    ).expect("TODO: panic message");
+    write(REPORT_FILE_NAME, to_string_pretty(&json_to_write).unwrap())
+        .expect("TODO: panic message");
 }
