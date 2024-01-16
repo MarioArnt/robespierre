12c0ae214163b25c7004a712d0b5e6f5914edeb4
Mario Arnautou <mario.arnautou@gmail.com>
2024-01-16 14:19:00 +0100

    chore: fix versioning script to support pre-release

--- version.sh
+++ version.sh
@@ -1,7 +1,7 @@
 release=$1
-sed -Ei '' "s/\"version\": \"[0-9\.]+\"/\"version\": \"${release}\"/" ./npm/robespierre/package.json
-sed -Ei '' "s/\"robespierre-([a-z0-9-]+)\": \"[0-9\.]+\"/\"robespierre-\1\": \"${release}\"/g" ./npm/robespierre/package.json
-sed -Ei '' "s/^version = \"[0-9\.]+\"$/version = \"${release}\"/" ./Cargo.toml
+sed -Ei '' "s/\"version\": \"[0-9a-z-\.]+\"/\"version\": \"${release}\"/" ./npm/robespierre/package.json
+sed -Ei '' "s/\"robespierre-([a-z0-9-]+)\": \"[0-9a-z-\.]+\"/\"robespierre-\1\": \"${release}\"/g" ./npm/robespierre/package.json
+sed -Ei '' "s/^version = \"[0-9a-z-\.]+\"$/version = \"${release}\"/" ./Cargo.toml
 cat ./npm/robespierre/package.json
 cat ./Cargo.toml
 cargo build
