99c3b7bb4c085db0b1ee25d5d78d1d711bc9a56d
Mario Arnautou <mario.arnautou@gmail.com>
2024-01-16 10:28:01 +0100

    ci: fix sed in version script

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 release=$1
-sed "s/\"version\": \"[0-9\.]+\"/\"version\": \"${release}\"/" ./npm/robespierre/package.json
-sed "s/\"robespierre-([a-z0-9-]+)\": \"[0-9\.]+\"/\"robespierre-\1\": \"${release}\"/g" ./npm/robespierre/package.json
-sed "s/^version = \"[0-9\.]+\"$/version = \"${release}\"/" ./Cargo.toml
+sed -Ei '' "s/\"version\": \"[0-9\.]+\"/\"version\": \"${release}\"/" ./npm/robespierre/package.json
+sed -Ei '' "s/\"robespierre-([a-z0-9-]+)\": \"[0-9\.]+\"/\"robespierre-\1\": \"${release}\"/g" ./npm/robespierre/package.json
+sed -Ei '' "s/^version = \"[0-9\.]+\"$/version = \"${release}\"/" ./Cargo.toml
 cat ./npm/robespierre/package.json
 cat ./Cargo.toml
