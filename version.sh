1aa895c15f7e0f74707e1c3215688ca88f4d529c
Mario Arnautou <mario.arnautou@gmail.com>
2024-01-16 10:20:00 +0100

    ci: publish correct version automatically

---
+++ version.sh
@@ -0,0 +1,4 @@
+release=$1
+sed -Ei '' "s/\"version\": \"[0-9\.]+\"/\"version\": \"${release}\"/" ./npm/robespierre/package.json
+sed -Ei '' "s/\"robespierre-([a-z0-9-]+)\": \"[0-9\.]+\"/\"robespierre-\1\": \"${release}\"/g" ./npm/robespierre/package.json
+sed -Ei '' "s/^version = \"[0-9\.]+\"$/version = \"${release}\"/" ./Cargo.toml
