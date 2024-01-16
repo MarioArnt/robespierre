ca8a8220694875ea6a776d28fe6fd4159059f4ff
Thibault Ruby <thibault.ruby@neoxia.com>
2024-01-16 16:51:10 +0100

    chore: publish 1.0.0-beta.2 :tada:

--- version.sh
+++ version.sh
@@ -1,9 +1,11 @@
 release=$1
-sed -Ei '' "s/\"version\": \"[0-9a-z-\.]+\"/\"version\": \"${release}\"/" ./npm/robespierre/package.json
-sed -Ei '' "s/\"robespierre-([a-z0-9-]+)\": \"[0-9a-z-\.]+\"/\"robespierre-\1\": \"${release}\"/g" ./npm/robespierre/package.json
-sed -Ei '' "s/^version = \"[0-9a-z-\.]+\"$/version = \"${release}\"/" ./Cargo.toml
+echo "Updating files for $release release."
+sed -Ei '' "s/\"version\": \"([0-9a-zA-Z]|-|\.)+\"/\"version\": \"${release}\"/" ./npm/robespierre/package.json
+sed -Ei '' "s/\"robespierre-(.+)\": \"([0-9a-zA-Z]|-|\.)+\"/\"robespierre-\1\": \"${release}\"/g" ./npm/robespierre/package.json
+sed -Ei '' "s/^version = \"([0-9a-zA-Z]|-|\.)+\"$/version = \"${release}\"/" ./Cargo.toml
 cat ./npm/robespierre/package.json
 cat ./Cargo.toml
 cargo build
+# shellcheck disable=SC2164
 cd ./npm/robespierre
 npm i
