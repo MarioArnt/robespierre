3165712ca613b731ee2ec281b1bf6ff03e6b9b10
Mario Arnautou <mario.arnautou@gmail.com>
2024-01-17 12:10:39 +0100

    chore: release 1.0.0-beta.2 :tada:

--- package.sh
+++ package.sh
@@ -26,10 +26,9 @@ if [ "${BUILD_OS}" = "windows-2022" ]; then
   bin="${bin}.exe"
 fi
 cp "../target/${BUILD_TARGET}/release/${bin}" "${node_pkg}/bin"
-cp "../README.md" "${node_pkg}"
 # publish the package
 if [ "$1" = "--publish" ]; then
+  cp "../README.md" "${node_pkg}"
   cd "${node_pkg}"
   npm publish --access public
 fi
-rm "${node_pkg}/README.md"
